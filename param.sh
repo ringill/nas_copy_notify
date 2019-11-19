c3a8415539f3b5d56ffad7e93fb9e02479068775
Mikhail Guskov <ringill@mail.ru>
2019-11-20 01:54:02 +0300

    bugfix: - убрал ошибочны переменные из файла параметров

--- param.sh
+++ param.sh
@@ -4,6 +4,6 @@
 # задания на копирование
 declare -a jobs
 # "откуда" ; "куда" ; "1" - если нужно только содержимое папки
-jobs[0]="$build/Users/kea/Library/Mobile Documents/com~apple~CloudDocs/;$build/Volumes/Backup/IcloudDriveBackup;1"
-jobs[1]="$build/Volumes/Work/Фото Танюши;$build/Volumes/Backup_KTS;0"
-jobs[2]="$build/Volumes/Work/Документы Танюши;$build/Volumes/Backup_KTS;0"
+jobs[0]="/Users/kea/Library/Mobile Documents/com~apple~CloudDocs/;/Volumes/Backup/IcloudDriveBackup;1"
+jobs[1]="/Volumes/Work/Фото Танюши;/Volumes/Backup_KTS;0"
+jobs[2]="/Volumes/Work/Документы Танюши;/Volumes/Backup_KTS;0"
